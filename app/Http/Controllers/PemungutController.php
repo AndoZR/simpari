199badbfc09a78b4db07014e013860ee94e49393
Ando Zamhariro Royan <andozroyan850@gmail.com>
2025-11-27 07:24:24 +0700

    optimal4

--- app/Http/Controllers/PemungutController.php
+++ app/Http/Controllers/PemungutController.php
@@ -280,10 +280,11 @@ class PemungutController extends Controller
                         ]);
 
                     }
-                    $tagihan->increment('uang_dipemungut', $nominal); // update kolom uang_dipemungut karena masyarakat udah bayar cicilan
+                    // $tagihan->increment('uang_dipemungut', $nominal); // update kolom uang_dipemungut karena masyarakat udah bayar cicilan
                     $tagihan->update([
                         'status' => 'cicilan',
                         'sisa_tagihan' => $tagihan->jumlah - $nominal,
+                        'uang_dipemungut' => $nominal
                     ]);
                 }
             }
